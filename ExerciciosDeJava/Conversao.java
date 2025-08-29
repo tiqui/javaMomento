c592b04aaf97b408c0e9d659e7ff4859887c7740
R. Yuri <89981741+tiqui@users.noreply.github.com>
2025-08-29 12:59:57 -0300

    Add files via upload

--- ExerciciosDeJava/Conversao.java
+++ ExerciciosDeJava/Conversao.java
@@ -4,28 +4,21 @@ public class Conversao
 {
     public static void main(String[] args)
     {
-        Scanner entrada = new Scanner (System.in)
+        Scanner entrada = new Scanner (System.in);
                 
-        double Celsius=0;
+        double Cels=0;
         
+        System.out.print("Informe os graus em Cels: " );
+        Cels = entrada.nextDouble();
         
-        
-        
-        
-        
-        
+        double Faren = (Cels * 9/5) + 32;
+               
+        System.out.print("Em F: " + Faren);
         
         
     }
-
-
-
-
-
-   
+  
 }
-
-
 
 /*5) A conversão de graus Fahrenheit para Celsius é obtida pela fórmula abaixo. Faça um algoritmo que leia um
 valor em graus Centígrados e imprima seu correspondente em graus Fahrenheit. */
