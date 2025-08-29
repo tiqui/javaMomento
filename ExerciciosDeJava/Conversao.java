e23f618cbe9d0ce23440c3e797e6c75e8136b356
R. Yuri <89981741+tiqui@users.noreply.github.com>
2025-08-29 09:50:56 -0300

    Add files via upload

---
+++ ExerciciosDeJava/Conversao.java
@@ -0,0 +1,31 @@
+package flerovio.codigos;
+import java.util.Scanner;
+public class Conversao 
+{
+    public static void main(String[] args)
+    {
+        Scanner entrada = new Scanner (System.in)
+                
+        double Celsius=0;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+
+
+
+   
+}
+
+
+
+/*5) A conversão de graus Fahrenheit para Celsius é obtida pela fórmula abaixo. Faça um algoritmo que leia um
+valor em graus Centígrados e imprima seu correspondente em graus Fahrenheit. */
---
+++ ExerciciosDeJava/Gasolina.java
@@ -0,0 +1,36 @@
+package flerovio.codigos;
+import java.util.Scanner;
+public class Gasolina 
+{
+ public static void main(String[] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+        int KMi=0, KMf=0;
+        double ValorGasolina=0;
+        double MediaTotal=0.0;
+        
+        
+        
+        
+        System.out.print("Km inicial: ");
+        KMi = entrada.nextInt();
+        System.out.print("Km final:");
+        KMf = entrada.nextInt();
+        System.out.print("Quantos reais de gasolina: ");
+        ValorGasolina = entrada.nextInt();
+        
+        
+        /* consumo de combustivel */
+        MediaTotal = (KMf - KMi) / ValorGasolina;
+        
+        /* resultado */
+        System.out.print("O valor total de km por litro: "+MediaTotal);
+                             
+    }
+    
+}
+
+/* 2) Realizarei uma viagem de vários dias em meu automóvel, e gostaria de saber a quilometragem média por
+litro de gasolina. Para isto, anotarei a quilometragem no velocímetro ao sair de viagem, e depois ao chegar.
+Também vou somar toda a gasolina que comprar para o carro. Você poderia fazer um programa que me
+desse, com estes dados, quantos km fiz, em média, por litro de gasolina? */
